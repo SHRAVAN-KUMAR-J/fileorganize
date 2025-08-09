996031f9892190281e0b666cefda218421d29ab0
SHRAVAN-KUMAR-J <shravanjalu7034@gmail.com>
2025-08-09 18:19:05 +0530

    'sgdv'

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://backend-1-u77g.onrender.com/api',
 });
 
 export const uploadFiles = async (files, sortBy, sortOrder, minSize, maxSize) => {
